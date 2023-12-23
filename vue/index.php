12daadc568103d35c6fe10f37f5f8fd86683c9c1
alaeddinerami <alaeddinerami7@gmail.com>
2023-12-24 00:06:34 +0100

    ceart pseudoname  page

--- vue/index.php
+++ vue/index.php
@@ -1,29 +1,38 @@
 <?php
 require_once('../config/db.php');
 session_start();
-    if (isset($_POST['start'])){ 
-        $_SESSION['pseudo_name']= $_POST['pseudo_name'];
-        echo $_SESSION['pseudo_name'];
-        header('location:quiz.php');
-    }
+
+if (isset($_POST['start'])) {
+    $_SESSION['pseudo_name'] = $_POST['pseudo_name'];
+    header('location: quiz.php');
+    exit; // Make sure to exit after a header redirect
+}
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>start</title>
+    <title>Start</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
-<body class="h-screen bg-cover bg-center bg-no-repeat bg-fixed flex justify-center items-center" style="background-image: url('../images/2020_07_what_is_aws.jpg')">
-    <form method="post" class="max-w-md h-1/5 p-6 bg-blue-900 bg-opacity-20 rounded-md shadow-md flex flex-col justify-between">
-        <!-- Use the 'placeholder' attribute to set the pseudo name -->
-        <input type="text" name="pseudo_name" class="mt-1 p-2 w-full border-2 border-gray-300 rounded-md focus:outline-none focus:border-am-500" placeholder="Your Pseudo Name">
-        <button type="submit" name="start" class="mt-4 px-4 py-2 bg-amber-500 text-white rounded-md hover:bg-amber-700 focus:outline-none focus:bg-amber-700">Start</button>
-    </form>
+<body class="h-screen bg-cover bg-center bg-no-repeat bg-fixed backdrop-brightness-50 bg-[url('../images/2020_07_what_is_aws.jpg')]">
+    <section class="h-screen flex  items-center justify-around flex-col">
+        <img src="../images/quizx-removebg-preview.png" class="h-1/5 w-1/5" alt="">
+        <h1 class=" text-white text-2xl">"Let's embark on a journey of knowledge <br> and fun with our exciting quiz adventure!"</h1>
 
+        <form method="post" class="w-1/4 h-1/5 p-6 bg-blue-900 bg-opacity-20 rounded-md shadow-md flex flex-col justify-between">
+            <input type="text" name="pseudo_name" class="mt-1 p-2 w-full border-2 border-gray-300 rounded-md focus:outline-none focus:border-am-500" placeholder="Your Pseudo Name">
+            <button type="submit" name="start" class="mt-4 px-4 py-2 bg-amber-500 text-white rounded-md hover:bg-amber-700 focus:outline-none focus:bg-amber-700">Start</button>
+        </form>
+
+        <footer class=" text-white p-4 text-center">
+            <p>&copy; 2023 QuizX. All rights reserved.</p>
+        </footer>
+    </section>
 </body>
 
 </html>
--- vue/quiz.php
+++ vue/quiz.php
@@ -1,4 +1,66 @@
 <?php
+// require("../controller/question.php");
 session_start();
-echo $_SESSION["pseudo_name"];
+  if(empty($_SESSION["pseudo_name"])){
+    header('location:index.php');
+}else 
+{
+   $name= $_SESSION["pseudo_name"];
+    echo $name;
+}
+
+
 ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quiz Game</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+</head>
+
+<body class="bg-gray-100 h-screen flex items-center justify-center">
+
+    <div class="bg-white p-8 rounded-md shadow-md max-w-md w-full">
+
+        <!-- Question -->
+        <h2 class="text-2xl font-semibold mb-4">Question: What is the capital of France?</h2>
+
+        <!-- Answer Options Form -->
+        <form action="process_answer.php" method="post">
+
+            <div class="grid grid-cols-2 gap-4">
+                <!-- Option 1 -->
+                <label>
+                    <input type="radio" name="answer" value="paris" class="hidden">
+                    <button type="submit" class="py-2 px-4 bg-blue-500 text-white rounded-md hover:bg-blue-700 focus:outline-none focus:bg-blue-700">Paris</button>
+                </label>
+
+                <!-- Option 2 -->
+                <label>
+                    <input type="radio" name="answer" value="berlin" class="hidden">
+                    <button type="submit" class="py-2 px-4 bg-blue-500 text-white rounded-md hover:bg-blue-700 focus:outline-none focus:bg-blue-700">Berlin</button>
+                </label>
+
+                <!-- Option 3 -->
+                <label>
+                    <input type="radio" name="answer" value="madrid" class="hidden">
+                    <button type="submit" class="py-2 px-4 bg-blue-500 text-white rounded-md hover:bg-blue-700 focus:outline-none focus:bg-blue-700">Madrid</button>
+                </label>
+
+                <!-- Option 4 -->
+                <label>
+                    <input type="radio" name="answer" value="rome" class="hidden">
+                    <button type="submit" class="py-2 px-4 bg-blue-500 text-white rounded-md hover:bg-blue-700 focus:outline-none focus:bg-blue-700">Rome</button>
+                </label>
+            </div>
+
+        </form>
+
+    </div>
+
+</body>
+
+</html>
